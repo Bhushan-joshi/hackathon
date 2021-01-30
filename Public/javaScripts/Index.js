971ab72da528690b65b89377c0631c52609f7a6c
Aaditya Paithane <51539518+aadityamp01@users.noreply.github.com>
2021-01-30 15:02:40 +0530

    Header, login pages added

--- Public/javaScripts/Index.js
+++ Public/javaScripts/Index.js
@@ -1,3 +1,12 @@
 /**
  * TODO : add your  JavaScript logic for user interfaces
  */
+$(function () {
+    $(window).on('scroll', function () {
+        if ( $(window).scrollTop() > 10 ) {
+            $('.navbar').addClass('active');
+        } else {
+            $('.navbar').removeClass('active');
+        }
+    });
+});
