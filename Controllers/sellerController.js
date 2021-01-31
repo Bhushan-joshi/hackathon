13c994ca7fc1c3257f89bf49bf40e3648226c917
Bhushan Joshi <bhushanjoshi911@gmail.com>
2021-01-31 12:32:46 +0530

    add Product

--- Controllers/sellerController.js
+++ Controllers/sellerController.js
@@ -1,9 +1,22 @@
-const Product=require('../Models/product');
+const Product = require('../Models/product');
 
-exports.getIndex=(req,res,next)=>{
-	res.render('Seller/addProduct');
+exports.getIndex = (req, res, next) => {
+	res.render('Seller/addProduct',{
+		Sucess:null
+	});
 }
 
-exports.addProduct=(req,res,next)=>{
-	
+exports.addProduct = (req, res, next) => {
+	const { title, price, imageUrl, desc } = req.body;
+	const product = new Product({
+		Title: title,
+		Price: price,
+		Imageurl: imageUrl,
+		Description: desc,
+	});
+	product.save(savedProduct=>{
+		res.render('Seller/addProduct',{
+			Sucess:'producted added Sucessfully!',
+		})
+	})
 }
